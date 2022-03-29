2af413acb61dc881d795e5bef35716c82959e954
VivifyIdeas <vivify@VivifyIdeass-MacBook-Pro-8.local>
2022-03-29 21:46:34 +0200

    zadatak 5: 1/3 uradjeno

--- app/Http/Controllers/CarController.php
+++ app/Http/Controllers/CarController.php
@@ -12,10 +12,22 @@ class CarController extends Controller
 {
     public function index(Request $request)
     {
+        $brand = $request->query('brand');
+        $model = $request->query('model');
+
+
         // $cars = Car::all();
         $query = Car::query();
+
+        if ($brand) {
+            $query = $query->where('brand', '+', "%$brand%");
+        }
+        if ($model) {
+            $query = $query->where('model', 'like', "%$model%");
+        }
+
         $cars = $query->paginate(
-            $request->query('per_page', 5)); // page size
+            $request->query('per_page', 5));
 
         return response()->json($cars);
     }
